a11b2df1c7f023034cc37720ccd5cac4e10416db
patrickjcash <patrick.j.cash@gmail.com>
2018-05-23 12:02:24 -0400

    Initial upload

---
+++ email-tester.sh
@@ -0,0 +1,23 @@
+#
+# Batch email tester for terminal (mac)
+# v0.1 by Patrick Cash
+# 
+# Populate emails.txt with one test email per line
+# Use 'dig +short MX mydomain.com' to determine target
+#   mx server and populate below
+# Run from terminal and pipe output to file
+#
+
+
+cat emails.txt | while read i; do
+	echo $i
+	(
+		echo "HELO sample.com" # dummy, no need to replace
+		sleep 0.05
+		echo "MAIL FROM:<postman@sample.com>" # dummy, no need to replace
+		sleep 0.05
+		echo "RCPT TO:<"$i">"
+		sleep 0.05
+		echo "QUIT"
+	) | nc MX.MYDOMAIN.COM 25 # replace with target MX server
+done
